b9535f418548a9717a8569cbcdf5b63e5ec8c125
osman <osmnlhn@gmail.com>
2023-12-06 20:45:26 +0000

    first Commit

--- src/App.js
+++ src/App.js
@@ -5,22 +5,30 @@ import TaskForm from "./TaskForm";
 import TaskHookForm from "./TaskHookForm";
 import PeopleForm from "./PeopleForm";
 import { initialTasks, initialTeam } from "./data";
-
+import { toast, ToastContainer } from "react-toastify";
 
 function App() {
   const [tasks, setTasks] = useState(initialTasks);
   const [team, setTeam] = useState(initialTeam);
 
   function handleTaskSubmit(yeniTask) {
-    setTasks([yeniTask, ...tasks])
+    setTasks([yeniTask, ...tasks]);
+    toast("Yeni Gorev Eklendi!");
   }
 
   function handlePeopleSubmit(yeniKisi) {
-    setTeam([...team, yeniKisi])
+    setTeam([...team, yeniKisi]);
+    toast("Yeni Kisi Eklendi!");
   }
 
   function handleComplete(id) {
-    console.log("tamamlama fonksiyonunu buraya yazın")
+    // console.log("tamamlama fonksiyonunu buraya yazın");
+
+    const newTasks = tasks.map((task) =>
+      task.id === id ? { ...task, status: "yapıldı" } : task
+    );
+    setTasks(newTasks);
+    toast("Gorev Tamamlandi!");
   }
 
   return (
@@ -28,7 +36,7 @@ function App() {
       <div className="formColumn">
         <div className="form-container">
           <h2>Yeni Task</h2>
-          {/* <TaskForm kisiler={team} submitFn={handleTaskSubmit} /> */}
+          {<TaskForm kisiler={team} submitFn={handleTaskSubmit} />}
           <TaskHookForm kisiler={team} submitFn={handleTaskSubmit} />
         </div>
 
@@ -57,9 +65,20 @@ function App() {
                 <Task key={t.id} taskObj={t} />
               ))}
           </div>
+          <ToastContainer
+            position="top-right"
+            autoClose={6000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
         </div>
       </div>
-
     </div>
   );
 }
--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -1,7 +1,121 @@
-import React from 'react'
+import { nanoid } from "nanoid";
+import React from "react";
+import { useForm } from "react-hook-form";
 
-export default function TaskHookForm() {
+import "react-toastify/dist/ReactToastify.css";
+
+export default function TaskHookForm({ kisiler, submitFn }) {
+
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    reset,
+  } = useForm({
+    defaultValues: {
+      title: "",
+      description: "",
+      people: [],
+    },
+    mode: "all",
+
+  })
+
+
+ 
+
+  const taskSubmit =(data) => {
+    submitFn({...data, id:nanoid(5), status:"Done"});
+    console.log(data);
+    data.reset();
+  }
+
   return (
-    <div>Formunuzu react-hook-form kullanarak burada oluşturun. TaskForm dosyasındaki HTML yapısını vs app.css içerisindeki classları kullanabilirsiniz.</div>
-  )
+    <form className="taskForm" onSubmit={handleSubmit(taskSubmit)}>
+      <div className="form-line">
+        <label className="input-label" htmlFor="title">
+          Başlık
+        </label>
+        <input
+          className="input-text"
+          id="title"
+          name="title"
+          type="text"
+          {...register("title", {
+            required: { value: true, message: "Isim giriniz" },
+            minLength: { value: 3, message: "En az 3 karakter giriniz" }
+
+          })
+
+          }
+        />
+        {errors.title && <p className="input-error">{errors.title.message}</p>}
+      </div>
+   
+      <div className="form-line">
+        <label className="input-label" htmlFor="description">
+          Açıklama
+        </label>
+        <textarea
+          className="input-textarea"
+          rows="3"
+          id="description"
+          name="description"
+          placeholder="React Hook Form"
+          {...register("description", {
+            required: { value: true, message: "Aciklama giriniz" },
+            minLength: { value: 10, message: "En az 10 karakter giriniz" }
+          })} ></textarea>
+        {errors.description && (
+          <p className="input-error">{errors.description.message}</p>
+        )}
+      </div>
+
+      <div className="form-line">
+        <label className="input-label">İnsanlar</label>
+        <div>
+          {kisiler.map((p) => (
+            <label className="input-checkbox" key={p}>
+              <input
+                type="checkbox"
+                name="people"
+                value={p}
+                {...register("people", {
+                  validate: (value) => value.length > 0
+                    ? value.length <= 3 || "En fazla 3 kisi secebilirsiniz"
+                    : "En az 1 kisi seciniz",
+                })}
+              />
+              {p}
+            </label>
+          ))}
+        </div>
+        {errors.people && (
+          <p className="input-error">{errors.people.message}</p>
+        )}
+      </div>
+
+      <div className="form-line">
+        <button className="submit-button" type="submit" disabled={!isValid}>
+          Kaydet
+        </button>
+
+      </div>
+
+      <div style={{display:"flex", justifyContent:"center"}}>
+      <input
+        style={{marginTop: 20 , textAlign:"center"  }}
+        type="button"
+        onClick={() =>
+          reset({
+            title: "",
+            description: "",
+            people: [],
+          })
+        }
+        value="Reset with values"
+      />
+      </div>
+    </form>
+  );
 }
